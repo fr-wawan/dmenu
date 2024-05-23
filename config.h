6075c6e146d3d75f6d85e3bb14606051daba5e0b
Wawan <hermawantan12@gmail.com>
2024-05-23 11:51:57 +0800

    nord theme

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono Nerd Font:size=12",
+	"FiraMono Nerd Font:size=12",
 	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
@@ -12,9 +12,9 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#cdd6f4", "#1e1e2e" },
-	[SchemeSel] = { "#1e1e2e", "#89dceb" },
-	[SchemeOut] = { "#000000", "#89dceb" },
+	[SchemeNorm] = { "#D8DEE9", "#2E3440" },
+	[SchemeSel] = { "#ECEFF4", "#434C5E" },
+	[SchemeOut] = { "#000000", "#434C5E" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
